072d654c9bffe36a354b067ae62feeb9ca629fb1
Alberto <plagelao@gmail.com>
2011-02-03 20:09:47 +0000

    Removing some auto-generated specs

--- spec/views/shared/_header.haml_spec.rb
+++ spec/views/shared/_header.haml_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe "shared/_header.haml" do
   it "set the page title" do
     render :partial => 'shared/header'
-    response.should contain("Let's walk the path together!")
+    rendered.should contain("Let's walk the path together!")
   end
 end

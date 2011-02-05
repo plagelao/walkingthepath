92fbdae5856e2c30399b9f08356affca183b8600
Alberto <plagelao@gmail.com>
2011-02-05 20:30:14 +0000

    rendering the partials in a modern style :D

--- spec/views/shared/_header.haml_spec.rb
+++ spec/views/shared/_header.haml_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "shared/_header.haml" do
   it "set the page title" do
-    render :partial => 'shared/header'
+    render 'shared/header'
     rendered.should contain("Let's walk the path together!")
   end
 end

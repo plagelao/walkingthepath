b424d809e551cbf7c3f1a829a71af1f6ffbe163b
Alberto <plagelao@gmail.com>
2011-02-06 00:04:26 +0000

    Change in the event date partials to remove the event

--- spec/views/events/_event.haml_spec.rb
+++ spec/views/events/_event.haml_spec.rb
@@ -2,9 +2,27 @@ require 'spec_helper'
 
 describe "events/_event.haml" do
 
+  it "ask for the event id" do
+    event = mock_model(Event).as_null_object
+    event.should_receive(:id)
+    render 'events/event', :event => event
+  end
+
   it "ask for the event month" do
     event = mock_model(Event).as_null_object
     event.should_receive(:month)
+    render 'events/event', :event => event
+  end
+
+  it "ask for the event day" do
+    event = mock_model(Event).as_null_object
+    event.should_receive(:day)
+    render 'events/event', :event => event
+  end
+
+  it "ask for the event time" do
+    event = mock_model(Event).as_null_object
+    event.should_receive(:time)
     render 'events/event', :event => event
   end
 
--- spec/views/events/_event_date.haml_spec.rb
+++ spec/views/events/_event_date.haml_spec.rb
@@ -3,20 +3,17 @@ require 'spec_helper'
 describe "events/_event_date.haml" do
 
   it "ask for the event month" do
-    event = mock_model(Event).as_null_object
-    event.should_receive(:month)
-    render 'events/event_date', :event => event
+    render 'events/event_date', :month => 'Enero', :day => 2, :time => '20:00'
+    rendered.should have_selector('.month')
   end
 
   it "ask for the event day" do
-    event = mock_model(Event).as_null_object
-    event.should_receive(:day)
-    render 'events/event_date', :event => event
+    render 'events/event_date', :month => 'Enero', :day => 2, :time => '20:00'
+    rendered.should have_selector('.day')
   end
 
   it "ask for the event time" do
-    event = mock_model(Event).as_null_object
-    event.should_receive(:time)
-    render 'events/event_date', :event => event
+    render 'events/event_date', :month => 'Enero', :day => 2, :time => '20:00'
+    rendered.should have_selector('.time')
   end
 end

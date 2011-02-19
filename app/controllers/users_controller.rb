043920a94c01de23a91fd995a2bb25bdb8e7466a
Alberto <plagelao@gmail.com>
2011-02-19 22:56:57 +0000

    Trying to understand twitter authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def authentication
-    raise request.env["ominauth.auth"].inspect
+    puts request.env["ominauth.auth"].inspect
     redirect_to events_path
   end
 
